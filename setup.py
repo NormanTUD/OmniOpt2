c0e4dc1da0f508838a9246904acc01ba92df485a
Norman Koch <norman.koch@tu-dresden.de>
2024-03-05 09:46:55 +0100

    moved to custom repo

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+from pprint import pprint
 import os
 import site
 import sys
 from distutils.sysconfig import get_python_lib
 
 from setuptools import setup
+
+def dier (msg):
+    pprint(msg)
+    sys.exit(1)
 
 # Allow editable install into user site directory.
 # See https://github.com/pypa/pip/issues/7953.
@@ -16,9 +21,7 @@ overlay_warning = False
 if "install" in sys.argv:
     lib_paths = [get_python_lib()]
     if lib_paths[0].startswith("/usr/lib/"):
-        # We have to try also with an explicit prefix of /usr/local in order to
-        # catch Debian's custom user site-packages directory.
-        lib_paths.append(get_python_lib(prefix="/usr/local"))
+        print("You need to be in a virtuel environment or something similiar to install this package")
     for lib_path in lib_paths:
         existing_path = os.path.abspath(os.path.join(lib_path, "omniopt2"))
         if os.path.exists(existing_path):
