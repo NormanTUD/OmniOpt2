41f124edc6e0b39578800a1ccd131fcba55f2e30
Norman Koch <norman.koch@tu-dresden.de>
2024-03-05 09:39:58 +0100

    moved to custom repo

---
+++ setup.py
@@ -0,0 +1,69 @@
+import os
+import site
+import sys
+from distutils.sysconfig import get_python_lib
+
+from setuptools import setup
+
+# Allow editable install into user site directory.
+# See https://github.com/pypa/pip/issues/7953.
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+# Warn if we are installing over top of an existing installation. This can
+# cause issues where files that were deleted from a more recent OmniOpt2 are
+# still present in site-packages. See #18115.
+overlay_warning = False
+if "install" in sys.argv:
+    lib_paths = [get_python_lib()]
+    if lib_paths[0].startswith("/usr/lib/"):
+        # We have to try also with an explicit prefix of /usr/local in order to
+        # catch Debian's custom user site-packages directory.
+        lib_paths.append(get_python_lib(prefix="/usr/local"))
+    for lib_path in lib_paths:
+        existing_path = os.path.abspath(os.path.join(lib_path, "omniopt2"))
+        if os.path.exists(existing_path):
+            # We note the need for the warning here, but present it after the
+            # command is run, so it's more likely to be seen.
+            overlay_warning = True
+            break
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"{lib_folder}/requirements.txt"
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
+
+setup(name='omniopt',
+    version='0.1',
+    description='Automatic hyperparameter optimizer based on Ax/Botorch',
+    author='Norman Koch',
+    author_email='norman.koch@tu-dresden.de',
+    url='https://scads.ai/transfer-2/verfuegbare-software-dienste-en/omniopt/',
+    packages=['distutils', 'distutils.command'],
+    install_requires=install_requires
+)
+
+if overlay_warning:
+    sys.stderr.write(
+        """
+
+========
+WARNING!
+========
+
+You have just installed OmniOpt2 over top of an existing
+installation, without removing it first. Because of this,
+your install may now include extraneous files from a
+previous version that have since been removed from
+OmniOpt2. This is known to cause a variety of problems. You
+should manually remove the
+
+%(existing_path)s
+
+directory and re-install OmniOpt2.
+
+"""
+        % {"existing_path": existing_path}
+    )
